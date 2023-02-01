e430f0fb09ddb0b10c68de5289e5d56ecf92685e
Damien Delmas <kerdelos@gmail.com>
2023-02-02 00:17:49 +0100

    day 1 pb 1

--- 2021/day_01/Program.cs
+++ 2021/day_01/Program.cs
@@ -1,2 +1,3 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿var lines = File.ReadLines("input.txt").ToList();
+var pb1 = lines.Zip(lines.Skip(1), (first, second) => Int32.Parse(second) > Int32.Parse(first) ? 1 : 0).Sum();
+Console.WriteLine($"Problem one : {pb1}");
