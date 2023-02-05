1260bcaa5db18d0675cde5f4bf4809c09746e588
Damien Delmas <kerdelos@gmail.com>
2023-02-05 12:57:35 +0100

    day 1 pb 2

--- 2021/day_01/Program.cs
+++ 2021/day_01/Program.cs
@@ -1,3 +1,18 @@
 ﻿var lines = File.ReadLines("input.txt").ToList();
 var pb1 = lines.Zip(lines.Skip(1), (first, second) => Int32.Parse(second) > Int32.Parse(first) ? 1 : 0).Sum();
 Console.WriteLine($"Problem one : {pb1}");
+
+List<U> Windowed<T,U>(List<T> list, int windowSize, Func<List<T>,U> lambda)
+{
+    var result = new List<U>();
+    for (int i = 0; i <= list.Count() - windowSize; i++)
+    {   
+        result.Add(lambda(list.GetRange(i,windowSize))); 
+    }
+    return result;
+}
+
+var sums = Windowed(lines,3, (l) => l.Select(x => Int32.Parse(x)).Sum());
+var pb2 = Windowed<int,int>(sums, 2, (l) => l[1] > l[0] ? 1 : 0).Sum();
+
+Console.WriteLine($"Problem two : {pb2}");
